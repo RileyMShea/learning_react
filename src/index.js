dae00e098bbd8147abc2f6038ab426704ae0fdc0
Riley Shea <RileyMShea@gmail.com>
2020-01-22 16:06:24 -0500

    Before cli deployment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const name = 'Riley Shea'
+const element = <h1>Hello, {name}</h1>
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
